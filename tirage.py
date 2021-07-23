13a0bf4ce0507d375d8557ac5d5f7c3e13598763
Célestin Matte <celestin.matte@gmail.com>
2021-07-23 20:59:29 +0200

    Avoid using the same tuple twice (in progress)

--- tirage.py
+++ tirage.py
@@ -4,15 +4,37 @@
 import sys as trans
 import random
 import csv
+import os
+
+TUPLE_FILE='tuples.csv'
+PEOPLE_FILE='people.csv'
 
 def fill_people():
     people=set()
-    with open('people.csv', newline='') as csvfile:
+    with open(PEOPLE_FILE, newline='') as csvfile:
         for row in csvfile:
             people.add(row.strip())
     return people
 
-def create_tuples():
+def get_existing_tuples():
+    already_done=set()
+    if os.path.isfile(TUPLE_FILE):
+        with open(TUPLE_FILE) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                already_done.add("%s-%s" % (row[0], row[1]))
+    else:
+        print("Tuple file not found, creating it.")
+    return already_done
+
+def print_and_save_results(tuples):
+    with open(TUPLE_FILE, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for t in tuples:
+            print("%s -> %s" % (t[0], t[1]))
+            writer.writerow(t)
+
+def create_tuples(already_done):
     people=fill_people()
     left=people.copy()
     first=random.choice(tuple(left))
@@ -22,18 +44,17 @@ def create_tuples():
     while len(left) > 0:
         prev=choice
         choice=random.choice(tuple(left))
+        while("%s-%s" %(prev, choice) in already_done):
+            choice=random.choice(tuple(left))
         left.remove(choice)
         tuples.append((prev, choice))
     tuples.append((choice, first))
     return tuples
 
 def main():
-    tuples=create_tuples()
-    with open('tuples.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for t in tuples:
-            print("%s -> %s" % (t[0], t[1]))
-            writer.writerow(t)
+    already_done=get_existing_tuples()
+    tuples=create_tuples(already_done)
+    print_and_save_results(tuples)
 
 if __name__ == "__main__":
     main()
