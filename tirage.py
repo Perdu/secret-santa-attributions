a06b7e072a9806891429f99a4a23ccaa29855ff6
Célestin Matte <celestin.matte@gmail.com>
2021-07-24 09:39:09 +0200

    Prevent infinite loop when a solution doesn't work

--- tirage.py
+++ tirage.py
@@ -34,28 +34,46 @@ def print_and_save_results(tuples):
             print("%s -> %s" % (t[0], t[1]))
             writer.writerow(t)
 
-def create_tuples(already_done):
+def tuples_loop(people, already_done):
+    tuples=[]
+    left=people.copy()
+    first=random.choice(tuple(left))
+    choice=first
+    left.remove(choice)
+    while len(left) > 0:
+        prev=choice
+        choice=random.choice(tuple(left))
+        not_tested=left.copy()
+        while ("%s-%s" % (prev, choice) in already_done) and len(not_tested) > 0:
+            choice=random.choice(tuple(left))
+            if choice in not_tested:
+                not_tested.remove(choice)
+        if ("%s-%s" % (prev, choice) in already_done) and len(not_tested) == 0:
+            # Oops, this person has already been attributed to all remaining
+            # persons. Starting over.
+            print("Solution failed, starting over.")
+            return []
+        left.remove(choice)
+        tuples.append((prev, choice))
+    if "%s-%s" % (choice, first) in already_done:
+        # Oops, we can't close the loop because the last pairing has
+        # already been done. We have to start over.
+        print("Ending the loop failed, starting over.")
+        return []
+    tuples.append((choice, first))
+    return tuples
+
+def create_tuples(already_done, max_retries=1000):
     people=fill_people()
     ok=False
-    while not ok:
-        tuples=[]
-        left=people.copy()
-        first=random.choice(tuple(left))
-        choice=first
-        left.remove(choice)
-        while len(left) > 0:
-            prev=choice
-            choice=random.choice(tuple(left))
-            while("%s-%s" %(prev, choice) in already_done):
-                choice=random.choice(tuple(left))
-            left.remove(choice)
-            tuples.append((prev, choice))
-        if "%s-%s" %(choice, first) not in already_done:
-            # Oops, we can't close the loop because the last pairing has
-            # already been done. We have to start over.
-            tuples.append((choice, first))
-            ok=True
-            print("Ending the loop failed, starting over.")
+    retries=0
+    tuples=[]
+    while len(tuples) == 0 and retries < max_retries:
+        tuples=tuples_loop(people, already_done)
+        retries += 1
+    if retries == max_retries:
+        print("Failed finding a solution in %d tries." % max_retries)
+        trans.exit(1)
     return tuples
 
 def main():
